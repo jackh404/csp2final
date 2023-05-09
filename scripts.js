d5708f8114b8847e196d7bd62a78d4b4957b2790
jackh404 <jackhenza@gmail.com>
2023-05-09 16:44:09 -0400

    corrected issues, document code

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Define slide objects to be used in slideshows
 Slide = function(caption,url,alt,link)
 {
     this.url=url;
@@ -5,6 +6,9 @@ Slide = function(caption,url,alt,link)
     this.alt=alt;
     this.link=link;
 }
+
+/*Change the image, caption, alt text, and link based on the
+contents of the next slide object in order to advance the slideshow*/
 function slide($lider,list,way,$caption,$link,index)
 {
     index+=way;
